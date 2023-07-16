1d40e430c88d3301f2a552a2667b6aa9a027c408
beesham <sarendranauth.beesham@gmail.com>
2023-07-16 16:47:50 -0400

    Install LAMP server components

--- wordpress_lamp.sh
+++ wordpress_lamp.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sudo apt install apache2
+sudo yum update -y
+
+sudo yum install -y httpd24 php72 mysql57-server php72-mysqlnd
+sudo service httpd start
+sudo chkconfig httpd on
+
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+#test page
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
